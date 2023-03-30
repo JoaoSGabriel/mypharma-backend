e291eca636174d3a40bc1a9ee667dad2c8d59ed2
JoaoSGabriel <joaogabriel0359@gmail.com>
2023-03-30 19:29:38 -0300

    feat: implement prisma client and mongoAtlas

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,28 @@
 import chalk from "chalk";
 import cors from "cors";
 import express, { json } from "express";
+import prisma from "./configs/prisma";
 
 const app = express();
 
 app
   .use(cors())
   .use(json())
-  .get("/status", (req, res) => res.send("Oh. hey there, I'm OK!"));
+  .get("/status", (req, res) => res.send("Oh. hey there, I'm OK!"))
+  .get("/test", async (req, res) => {
+    try {
+      await prisma.product.create({
+        data: {
+          name: "Banana",
+          price: 13.0,
+        },
+      });
+
+      res.sendStatus(201);
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
 const port = process.env.PORT || 5001;
 app.listen(port, () =>
