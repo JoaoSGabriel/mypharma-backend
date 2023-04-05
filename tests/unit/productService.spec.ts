4093af94282c79861245016accf6027c3403de96
JoaoSGabriel <joaogabriel0359@gmail.com>
2023-04-05 09:20:34 -0300

    feat: implement product factory

--- tests/unit/productService.spec.ts
+++ tests/unit/productService.spec.ts
@@ -1,5 +1,22 @@
 import { jest } from "@jest/globals";
 
+import productService from "@/services/product.service";
+import productRepository from "@/repositories/product.repository";
+
+import { products } from "../factories/product.factory";
+
 describe("Product searcher unit test suite", () => {
   it("should return products in alphabetic order", () => {});
+
+  it("should return products in rising order", () => {});
+
+  it("should return all products", () => {});
+
+  it("should return one category products in alphabetic order", () => {});
+
+  it("should return one category products in rising order", () => {});
+
+  it("should return one category products", () => {});
+
+  it("should return products thats contains the word", () => {});
 });
